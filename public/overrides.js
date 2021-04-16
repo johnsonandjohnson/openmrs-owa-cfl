596478e186813e266fa2fc0845fe7c04737460a1
Jakub Kondrat <jkondrat@soldevelo.com>
2021-04-16 16:53:01 +0200

    AGRE-1541: Patient Dashboard customizations (WIP)

--- public/overrides.js
+++ public/overrides.js
@@ -1,6 +1,19 @@
 $(function() {
+    /** Home **/
     const breadcrumbs = $("#breadcrumbs");
     if (breadcrumbs.is(':empty')) {
         $("#breadcrumbs").append("<span><a href=\"/openmrs/\">Home</a><span class=\"separator\">&gt;&gt;</span></span>");
+    }
+    /** Patient Dashboard **/
+    const firstInfoContainer = $(".info-container:first-of-type");
+    if (!firstInfoContainer.is(':empty')) {
+        const remainingContainersChildren = $(".info-container:not(:first-of-type) > [privilege] > *, .info-container:not(:first-of-type) > *");
+        remainingContainersChildren.detach().appendTo(firstInfoContainer);
+    }
+    const patientHeader = $(".patient-header");
+    if (!patientHeader.is(':empty')) {
+        const givenName = $(".PersonName-givenName").text();
+        const middleName = $(".PersonName-middleName").text();
+        const familyName = $(".PersonName-familyName").text();
     }
 });
