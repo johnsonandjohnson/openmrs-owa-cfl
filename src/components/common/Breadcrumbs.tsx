f8b93806a4032b9621693d91992ee3938663d145
Jakub Kondrat <jkondrat@soldevelo.com>
2021-04-20 13:52:06 +0200

    AGRE-1541: Fix linking on the additional breadcrumb

--- src/components/common/Breadcrumbs.tsx
+++ src/components/common/Breadcrumbs.tsx
@@ -5,32 +5,48 @@ import "./Breadcrumbs.scss";
 import { routeConfig } from "../Routes";
 import { FormattedMessage } from "react-intl";
 import { nameParam, redirectUrl } from "../../shared/util/url-util";
+import { ROOT_URL } from "../../shared/constants/openmrs";
+
+const separator = (showArrows = true) => (
+  <span className="separator">{showArrows && ">>"}</span>
+);
 
 const Breadcrumbs = (props) => {
   const breadcrumbs = useBreadcrumbs(routeConfig);
+  const url = redirectUrl(props.location.search);
+  const name = nameParam(props.location.search);
   return (
     <div className="breadcrumbs">
       {breadcrumbs.map(({ match, key, breadcrumb }, i) => {
         const id =
           breadcrumb && breadcrumb["props"] && breadcrumb["props"].children;
+        const isFirstBreadcrumb = i === 0;
+        const isLastBreadcrumb = breadcrumbs.length === i + 1;
         return (
           id.indexOf(".") >= 0 && (
             <span key={key}>
-              {i === 0 ? (
-                <a href={redirectUrl(props.location.search)}>
-                  {nameParam(props.location.search) || (
-                    <FormattedMessage id={"home.title"} />
+              {isFirstBreadcrumb ? (
+                <a href={ROOT_URL}>{<FormattedMessage id={"home.title"} />}</a>
+              ) : isLastBreadcrumb ? (
+                <>
+                  {url && name && (
+                    <>
+                      <a href={url}>{name}</a>
+                      {separator()}
+                    </>
                   )}
-                </a>
+                  <a href={window.location.href}>
+                    <FormattedMessage id={id} />
+                  </a>
+                </>
               ) : (
-                <Link to={match.url}>
-                  <FormattedMessage id={id} />
-                </Link>
+                <>
+                  <Link to={match.url}>
+                    <FormattedMessage id={id} />
+                  </Link>
+                </>
               )}
-              <span className="separator">
-                {(breadcrumbs.length === 1 || breadcrumbs.length !== i + 1) &&
-                  ">>"}
-              </span>
+              {separator(breadcrumbs.length === 1 || !isLastBreadcrumb)}
             </span>
           )
         );
--- src/shared/util/url-util.tsx
+++ src/shared/util/url-util.tsx
@@ -3,7 +3,6 @@ import { ROOT_URL } from "../constants/openmrs";
 
 export const redirectUrl = (search) => {
   const params = queryString.parse(search);
-  console.log(params, search);
   const redirect = params["redirect"];
   return (redirect || ROOT_URL).toString();
 };
