5aecffe99f93914f1497be427b348ddb9d98d2ec
Jakub Kondrat <jkondrat@soldevelo.com>
2021-05-12 17:46:24 +0200

    AGRE-1550: Remove unused PagedTable imports

--- src/components/find-caregiver/FindCaregiver.tsx
+++ src/components/find-caregiver/FindCaregiver.tsx
@@ -9,7 +9,6 @@ import searchIcon from '../../assets/img/search.png';
 import { columnContent } from '../../shared/util/cfl-person-util';
 import { DEFAULT_FIND_CAREGIVER_TABLE_COLUMNS } from '../../shared/constants/patient';
 import { SEARCH_INPUT_DELAY, SEARCH_INPUT_MIN_CHARS } from '../../shared/constants/input';
-import PagedTable from '../common/PagedTable';
 import { DEFAULT_PAGE_SIZE, pageOf } from '../../redux/page.util';
 import { helperText } from '../../shared/util/table-util';
 import { PATIENT_PAGE_URL } from '../../shared/constants/openmrs';
