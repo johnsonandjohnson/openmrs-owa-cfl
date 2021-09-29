b0b07702193e2f07f392c3bf0cc4bc8e2daa8c93
rstencel <rstencel@soldevelo.com>
2021-09-29 13:52:04 +0200

    AGRE-1839: Save regimen name without leading and trailing whitespaces

--- src/components/vmp-config/VmpConfig.tsx
+++ src/components/vmp-config/VmpConfig.tsx
@@ -151,6 +151,7 @@ class VmpConfig extends React.Component<IVmpConfigProps, IVmpConfigState> {
     if (!!config.vaccine) {
       config.vaccine = config.vaccine.filter(vc => !!vc.name);
       config.vaccine.forEach(vc => {
+        vc.name = vc.name.trim();
         vc.manufacturers = !!vc.manufacturers ? vc.manufacturers.filter(vcm => !!vcm) : [];
       });
     }
