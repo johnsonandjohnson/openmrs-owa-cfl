6101ab80fc0cae7b4b9563a98a7351ec552628bc
Kowalski, Marcin [JRDBE NON-J&J] <MKowals7@its.jnj.com>
2022-04-04 13:38:42 +0200

    AGRE-2217 Improvements after CR

--- src/redux/reducers/user.ts
+++ src/redux/reducers/user.ts
@@ -86,7 +86,7 @@ const reducer = (state = initialState, action) => {
 
       return {
         ...state,
-        updatedUser: updatedUser.data,
+        updatedUser: updatedUser?.data,
         success: {
           ...state.success,
           createUser: true
