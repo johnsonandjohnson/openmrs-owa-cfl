277983221e9be16ab755fdc4d46ed22ff62d1dd6
Piotr Wargulak <pwargulak@soldevelo.com>
2022-09-05 16:17:24 +0200

    AGRE-2444: Registration Form configuration extensions
    - added static field type which allows to 'silently' set property values in a new Patient
    - added configuration for the URL to redirect after successful registration

--- src/shared/constants/openmrs.tsx
+++ src/shared/constants/openmrs.tsx
@@ -13,3 +13,5 @@ export const WS_REST_V1_URL = `${ROOT_URL}ws/rest/v1/`;
 export const PURGE_TRUE_FLAG = 'purge=true';
 export const PATIENT_PAGE_URL = `${ROOT_URL}coreapps/clinicianfacing/patient.page`;
 export const CONDITIONS_PAGE_URL = `${ROOT_URL}coreapps/conditionlist/manageConditions.page`;
+/** Default URL to redirect after successful registration. */
+export const DEFAULT_REGISTRATION_FORM_REDIRECT = `${PATIENT_PAGE_URL}?patientId=%PATIENT_ID%&dashboard=%PERSON_TYPE%`;
