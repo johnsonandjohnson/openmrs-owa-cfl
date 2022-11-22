963b62856afb1c7b4e63abdd351b3d58d0bc6bc5
Piotr Wargulak <pwargulak@soldevelo.com>
2022-11-22 13:37:19 +0100

    AGRE-2615: Added proper locale selection for translation system

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -11,24 +11,38 @@
 import React from 'react';
 import './App.scss';
 import Routes from './Routes';
-import { connect } from 'react-redux';
-import { getSession } from '../redux/reducers/session';
-import { TinyButton as ScrollUpButton } from 'react-scroll-up-button';
+import {connect} from 'react-redux';
+import {getSession} from '../redux/reducers/session';
+import {TinyButton as ScrollUpButton} from 'react-scroll-up-button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-3/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../assets/css/font-awesome-3.0.2.min.css';
-import { getApps } from '../redux/reducers/apps';
+import {getApps} from '../redux/reducers/apps';
 import '@openmrs/style-referenceapplication/lib/referenceapplication.css';
-import { toast } from 'react-toastify';
+import {toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { PROJECT_LOCATION_ATTRIBUTE_TYPE_NAME } from 'src/shared/constants/app';
+import {PROJECT_LOCATION_ATTRIBUTE_TYPE_NAME} from 'src/shared/constants/app';
+import {IntlProvider} from "react-intl";
+import {merge} from 'lodash';
+import flatten from 'flat';
+import messagesEN from '../lang/en.json'
+import messagesFR from '../lang/fr.json'
+import messagesPTBR from '../lang/pt_BR.json'
 
 toast.configure();
 
-export interface IAppProps extends StateProps, DispatchProps {}
+export interface IAppProps extends StateProps, DispatchProps {
+}
 
 class App extends React.Component<IAppProps> {
+  defaultLocale = 'en';
+  localeMessages = {
+    en: flatten(messagesEN),
+    fr: flatten(messagesFR),
+    'pt-BR': flatten(messagesPTBR)
+  };
+
   componentDidMount() {
     this.props.getSession();
   }
@@ -53,22 +67,36 @@ class App extends React.Component<IAppProps> {
     return projectName;
   }
 
+  // Translation fallback:
+  // locale -> locale without region codes -> default locale without region codes
+  getMessagesForLocale = (locale) => {
+    const localeWithoutRegionCode = locale.toLowerCase().split(/[_-]+/)[0];
+    const defaultLocaleWithoutRegionCode = this.defaultLocale.toLowerCase().split(/[_-]+/)[0];
+    return merge({}, this.localeMessages[defaultLocaleWithoutRegionCode], this.localeMessages[localeWithoutRegionCode], this.localeMessages[locale]);
+  };
+
   render() {
+    const locale = this.props.locale ? this.props.locale : this.defaultLocale;
+    const messages = this.getMessagesForLocale(locale);
+
     return (
-      <div id="app" className="app">
-        <Routes />
-        <ScrollUpButton />
-      </div>
+      <IntlProvider locale={locale} messages={messages}>
+        <div id="app" className="app">
+          <Routes/>
+          <ScrollUpButton/>
+        </div>
+      </IntlProvider>
     );
   }
 }
 
-const mapStateToProps = ({ session }) => ({
+const mapStateToProps = ({session}) => ({
   session: session.session,
-  userLocation: session.session?.sessionLocation
+  userLocation: session.session?.sessionLocation,
+  locale: session.session?.locale.replace("_", "-")
 });
 
-const mapDispatchToProps = { getSession, getApps };
+const mapDispatchToProps = {getSession, getApps};
 
 type StateProps = ReturnType<typeof mapStateToProps>;
 type DispatchProps = typeof mapDispatchToProps;
