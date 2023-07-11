a839f0ad95caf864e9e77db656b6c12f4f7d699f
Jozefow, Michal [JRDBE NON-J&J] <MJozefow@its.jnj.com>
2023-07-11 12:09:09 -0400

    Pull request #34: AGRE-2961:Added override for allergies tab breadcrumbs to unify with others, restored some code moved to cfl-module
    Merge in ITX-BPV/openmrs-owa-cfl from feature/AGRE-2961 to development
    
    Squashed commit of the following:
    
    commit 44a0834f7216bb207e4aa1d609dc44c08b1a8847
    Author: jozefowm <mjozefow@soldevelo.com>
    Date:   Tue Jul 11 15:37:29 2023 +0200
    
        AGRE-2961:Added override for allergies tab breadcrumbs to unify with others, restored some code moved to cfl-module

--- public/overrides.js
+++ public/overrides.js
@@ -7,10 +7,10 @@
  * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
  * graphic logo is a trademark of OpenMRS Inc.
  */
-
+ 
 const UNKNOWN_AGE = "unknown";
 const UNKNOWN_GENDER = "U";
-
+ 
 // Vanilla JS overrides - both for core OpenMRS and OWAs
 window.addEventListener('load', addCollapseToTheHeader);
 // Override Patient Header both on load and on page change (OWAs)
@@ -18,7 +18,63 @@ window.addEventListener('load', () => {
   overridePatientHeader();
   watchElementMutations('.patient-header:not(.custom)', overridePatientHeader);
 });
-
+ 
+// JQuery overrides (only for core OpenMRS)
+const jqr = (typeof $ === 'function' || typeof jQuery === 'function') && ($ || jQuery);
+jqr &&
+  jqr(function () {
+    // replace the url of 'Patient profile', 'Caregiver profile' and 'Conditions'
+    const searchParams = new URLSearchParams(window.location.search);
+    if (searchParams.has('patientId')) {
+      const givenName = document.querySelector('.PersonName-givenName')?.textContent;
+      const middleName = document.querySelector('.PersonName-middleName')?.textContent;
+      const familyName = document.querySelector('.PersonName-familyName')?.textContent;
+      const fullName = [givenName, middleName, familyName].join(' ').replace('  ', ' ');
+      const patientProfileAnchor = document.querySelector('a#cfl\\.patientProfile');
+      const deletePerson =
+        document.querySelector('#org\\.openmrs\\.module\\.coreapps\\.deletePatient') ||
+        document.querySelector('#cfl\\.personDashboard\\.deletePerson');
+      const uuidMatch = /'([^)]+)'/.exec(deletePerson?.href);
+      const patientId = (uuidMatch && uuidMatch[1]) || searchParams.get('patientId');
+      if (!!patientProfileAnchor) {
+        patientProfileAnchor.href = `${CFL_UI_BASE}index.html#/edit-patient/${patientId}?redirect=${window.location.href}&name=${fullName}`;
+      }
+      const caregiverProfileAnchor = document.querySelector('a#cfl\\.caregiverProfile');
+      if (!!caregiverProfileAnchor) {
+        caregiverProfileAnchor.href = `${CFL_UI_BASE}index.html#/edit-caregiver/${patientId}?redirect=${window.location.href}&name=${fullName}`;
+      }
+      const conditionsAnchor = document.querySelector('a#cfl\\.overallActions\\.conditions');
+      if (!!conditionsAnchor) {
+        conditionsAnchor.href = `${CFL_UI_BASE}index.html#/conditions/${patientId}`;
+      }
+      const conditionsIcon = document.querySelector('.info-section.conditions i.edit-action');
+      if (!!conditionsIcon) {
+        conditionsIcon.setAttribute('onclick', `location.href = '${CFL_UI_BASE}index.html#/conditions/${patientId}'`);
+      }
+    }
+    
+    // Add hamburger menu for general actions (visible on smaller screens)
+    const actionContainer = jqr('.action-container');
+    if (actionContainer.length) {
+      const actions = actionContainer.find('.action-section ul li');
+      actionContainer.before(
+        [
+          '<div class="general-actions-toggle navbar-dark">',
+          '<button class="navbar-toggler btn btn-secondary" type="button" data-toggle="collapse" data-target="#generalActions" aria-controls="generalActions" aria-expanded="false" aria-label="Toggle general actions">',
+          '<span class="navbar-toggler-icon mr-1"></span><span>General Actions</span>',
+          '</button>',
+          '<div class="collapse navbar-collapse" id="generalActions">',
+          actions
+            .toArray()
+            .map(action => '<div>' + action.innerHTML + '</div>')
+            .join('\n'),
+          '</div>',
+          '</div>'
+        ].join('\n')
+      );
+    }
+  });
+ 
 function addCollapseToTheHeader() {
   elementReady('.user-options').then(userOptions => {
     const collapse = window.document.getElementById('navbarSupportedContent');
@@ -38,19 +94,19 @@ function addCollapseToTheHeader() {
     }
   });
 }
-
+ 
 function isAgeAndGenderKnown(age, gender) {
   return age != UNKNOWN_AGE && gender != UNKNOWN_GENDER;
 }
-
+ 
 function isAgeUnknownAndGenderKnown(age, gender) {
   return age == UNKNOWN_AGE && gender != UNKNOWN_GENDER;
 }
-
+ 
 function isAgeKnownAndGenderUnknown(age, gender) {
   return age != UNKNOWN_AGE && gender == UNKNOWN_GENDER;
 }
-
+ 
 function getAgeAndGenderLabelText(age, gender) {
   if (isAgeAndGenderKnown(age, gender)) {
     return ' (' + age+ '/' + gender + ')';
@@ -60,7 +116,7 @@ function getAgeAndGenderLabelText(age, gender) {
     return ' (' + age + ')';
   } else return "";
 }
-
+ 
 function overridePatientHeader() {
   const patientHeader = document.querySelector('.patient-header:not(.custom)');
   // re-design Patient header
@@ -93,7 +149,7 @@ function overridePatientHeader() {
       if (status.length) {
         personStatus.textContent = status[1]?.trim();
       }
-
+ 
       var htmlLines = [
         '<div class="patient-header custom">',
         '<div class="patient-data"><h1>' + fullName + ageAndGender + '</h1>',
@@ -159,17 +215,34 @@ function overridePatientHeader() {
       } else {
         patientHeader.replaceWith(...updatedHeader);
       }
-
+ 
       if(!new URL(window.location.href).pathname.includes("/htmlformentryui/htmlform")) {
         // add (age/gender) to the breadcrumb
         elementReady('#breadcrumbs li:last-child:not(:empty)').then(element => {
           element.textContent = shownName + ageAndGender;
         });
       }
+
+      // Customize breadcrumbs for the allergies page
+      if (new URL(window.location.href).pathname.includes("/allergyui")) {
+        const homeBreadcrumb = $('#breadcrumbs li:first-child:not(:empty)');
+        const nameBreadCrumb = homeBreadcrumb.next();
+
+        const urlParams = new URLSearchParams(window.location.search);
+        const patientUuid = urlParams.get('patientId');
+        const patientDashboardURL = '/openmrs/coreapps/clinicianfacing/patient.page?patientId=' + patientUuid;
+
+        const nameBreadCrumbWrapper = $('<a>', {
+          href: patientDashboardURL,
+          text: fullName + ' '
+        });
+
+        nameBreadCrumb.empty().append(nameBreadCrumbWrapper);
+      }
     });
   }
 }
-
+ 
 /**
  * @return {NodeList}
  * @param htmlString
@@ -179,10 +252,10 @@ function htmlToElements(htmlString) {
   template.innerHTML = htmlString;
   return template.content.childNodes;
 }
-
+ 
 // MIT Licensed
 // Author: jwilson8767
-
+ 
 /**
  * Waits for an element satisfying selector to exist, then resolves promise with the element.
  * Useful for resolving race conditions.
@@ -213,7 +286,7 @@ function elementReady(selector, parentElement = document, notEmpty = false) {
     });
   });
 }
-
+ 
 /**
  * Observes changes to DOM starting from {@code parentElement} and its children, then calls {@code callback} on all elements
  * fitting {@code selector} once a change is detected.
@@ -235,4 +308,3 @@ function watchElementMutations(selector, callback, parentElement = document) {
     subtree: true
   });
 }
-
