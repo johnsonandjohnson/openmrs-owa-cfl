e00ae1f59a537e6884915b276888826ff61a70f1
Wargulak, Piotr [RNDBE NON-J&J] <PWargula@its.jnj.com>
2023-12-15 07:50:30 -0500

    Pull request #46: AGRE-2870: Improvements to birthday registration fields
    Merge in ITX-BPV/openmrs-owa-cfl from feature/AGRE-2870 to development
    
    Squashed commit of the following:
    
    commit 31a9fb5778e9f10a711612fe1cbe94763edc473c
    Author: Piotr Wargulak <pwargulak@soldevelo.com>
    Date:   Fri Dec 15 13:42:20 2023 +0100
    
        AGRE-2870: Changes after code review
    
    commit 7c0cf8c83eeb86667d2389e7fa3a19a27e93225e
    Author: Piotr Wargulak <pwargulak@soldevelo.com>
    Date:   Thu Dec 14 16:09:37 2023 +0100
    
        AGRE-2870: Improvements to birthday registration fields

--- src/components/register/Step.tsx
+++ src/components/register/Step.tsx
@@ -20,6 +20,7 @@ import { searchLocations } from '../../redux/reducers/location';
 import { getPhoneNumberWithPlusSign } from '../../shared/util/person-util';
 import { EMPTY_STRING, INPUT_ADDRESS_FIELDS_TYPE } from '../../shared/constants/input';
 import { GLOBAL_PROPERTY } from '../../shared/constants/concept';
+import { getDateFromModel } from "./inputs/DateInput";
 
 export interface IPatientIdentifierType {
   format: string;
@@ -50,6 +51,7 @@ export const LOCATIONS_OPTION_SOURCE = 'locations';
 export const SEPARATOR_FIELD_TYPE = 'separator';
 export const RELATIVES_FIELD_TYPE = 'relatives';
 export const PHONE_FIELD_TYPE = 'phone';
+export const DATE_FIELD_TYPE = 'date';
 export const STATIC_FIELD_TYPE = 'static';
 export const BIRTHDATE_FIELD = 'birthdate';
 export const ESTIMATED_BIRTHDATE_FIELDS = ['birthdateYears', 'birthdateMonths'];
@@ -125,38 +127,70 @@ export class Step extends React.Component<IStepProps, IStepState> {
     const regex = field.regex || patientIdentifierType?.format;
     let isInvalid = field.required && !value;
 
+    if (regex) {
+      const re = new RegExp(regex);
+      const isRequired = field.required || patientIdentifierType?.required;
+      isInvalid = !isRequired && !value ? false : !re.test(value);
+    }
+
+    if (field.type === DATE_FIELD_TYPE && !!patient[field.name]) {
+      const fieldDate = getDateFromModel(patient, field.name);
+
+      if (this.isPastDateNotAllowed(field) && fieldDate > new Date()) {
+        isInvalid = true;
+      }
+
+      if (this.isFutureDateNotAllowed(field) && fieldDate < new Date()) {
+        isInvalid = true;
+      }
+    }
+
     if (field.type === PHONE_FIELD_TYPE && !!value) {
       isInvalid = !isPossiblePhoneNumber(getPhoneNumberWithPlusSign(value));
     }
 
-    if (regex) {
-      const re = new RegExp(regex);
-      const isRequired = field.required || patientIdentifierType.required;
-      isInvalid = !isRequired && !value ? false : !re.test(value);
-    }
-
-    if (BIRTHDATE_FIELD === field.name || ESTIMATED_BIRTHDATE_FIELDS.includes(field.name)) {
-      // if estimation was made, don't require exact birthdate
-      const usesEstimate = ESTIMATED_BIRTHDATE_FIELDS.every(fieldName => !!this.props.patient[fieldName]);
-
-      if (usesEstimate) {
-        return false;
-      }
-    }
-
     const isGlobalPropertyOptionSource = field.optionSource === GLOBAL_PROPERTY;
-    const foundGlobalPropert = this.props.settings?.settings.find(({ property }) => property === field.optionUuid);
+    const foundGlobalProperty = this.props.settings?.settings.find(({ property }) => property === field.optionUuid);
 
-    if (isGlobalPropertyOptionSource && foundGlobalPropert?.value) {
-      const configParsed = JSON.parse(foundGlobalPropert.value);
+    if (isGlobalPropertyOptionSource && foundGlobalProperty?.value) {
+      const configParsed = JSON.parse(foundGlobalProperty.value);
 
       if (field.type === INPUT_ADDRESS_FIELDS_TYPE) {
         const addressFields = configParsed[field.optionKey][this.props.patient.country]?.map(({ field }) => field);
 
         return field?.allAddressFieldsRequired ? !addressFields?.every(field => this.props.patient[field]) : false;
       }
-    } else {
-      // Do nothing
+    }
+
+    return this.handleBirthdayFields(field, isInvalid);
+  };
+
+  isPastDateNotAllowed = field => field.allowFutureDates != undefined && !field.allowFutureDates;
+
+  isFutureDateNotAllowed = field => field.allowPastDates != undefined && !field.allowPastDates;
+
+  handleBirthdayFields = (field, isInvalid) => {
+    // Handle only when empty
+    if (this.props.patient[field.name]) {
+      return isInvalid;
+    }
+
+    if (BIRTHDATE_FIELD === field.name) {
+      // if estimation was made, don't require exact birthday
+      const usesEstimate = ESTIMATED_BIRTHDATE_FIELDS.some(fieldName => !!this.props.patient[fieldName]);
+
+      if (usesEstimate) {
+        return false;
+      }
+    }
+
+    if (ESTIMATED_BIRTHDATE_FIELDS.includes(field.name)) {
+      // if birthday was made, don't require exact estimation
+      const usesExact = !!this.props.patient[BIRTHDATE_FIELD];
+
+      if (usesExact) {
+        return false;
+      }
     }
 
     return isInvalid;
@@ -217,7 +251,7 @@ export class Step extends React.Component<IStepProps, IStepState> {
               const additionalProps = {} as any;
               const patientIdentifierType = this.getPatientIdentifierType(field);
 
-              if (i === stepDefinition.fields.filter(field => field.type !== STATIC_FIELD_TYPE).length - 1 || 
+              if (i === stepDefinition.fields.filter(field => field.type !== STATIC_FIELD_TYPE).length - 1 ||
                 (field.name === BIRTHDATE_FIELD && !!patient[BIRTHDATE_FIELD])) {
                 additionalProps.onKeyDown = this.handleLastFieldKeyDown;
               }
